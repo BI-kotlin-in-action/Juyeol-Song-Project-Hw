8b49638a8f5b4e55eb8a726f7b5899a825e49e14
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-07 12:57:27 +0900

    Comment: 주석 추가

--- lotto-project/src/main/kotlin/winninglotto/WinningPrize.kt
+++ lotto-project/src/main/kotlin/winninglotto/WinningPrize.kt
@@ -4,6 +4,11 @@ package winninglotto
  * @author : Unagi_zoso
  * @date : 2023-11-07
  */
+
+/**
+ * WinningPrize
+ * 당첨 등수와 당첨 금액을 가지고 있는 enum class
+ */
 enum class WinningPrize(val level: String, val prizeAmount: Int) {
     FIRST("1등", 100_000),
     SECOND("2등", 5_000),
