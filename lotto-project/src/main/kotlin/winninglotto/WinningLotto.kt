fd7c57b9c529191ce95eeb9003fad076bddc14cf
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-07 12:57:00 +0900

    Refactor: 메서드 명 변경 27번 줄 getMatchedNumbers -> getMatchedNumberSet

--- lotto-project/src/main/kotlin/winninglotto/WinningLotto.kt
+++ lotto-project/src/main/kotlin/winninglotto/WinningLotto.kt
@@ -12,15 +12,27 @@ import winninglotto.WinningPrize.THIRD
  * @author : Unagi_zoso
  * @date : 2023-11-06
  */
-object WinningLotto {
-    val lotto = AutomaticLottoMachineStrategy().generateLotto()
 
-    private fun getMatchedNumbers(otherLotto: Lotto): Set<Int> {
-        return lotto.numbers.intersect(otherLotto.numbers)
+/**
+ * WinningLotto
+ * 당첨 번호를 가지고 있는 객체 (Singleton)
+ * 당첨 번호를 생성하고, 당첨 번호와 비교하여 당첨 결과를 반환한다.
+ */
+object WinningLotto {
+    val winningLotto = AutomaticLottoMachineStrategy().generateLotto()
+
+    /**
+     * otherLotto와 winningLotto의 번호를 비교하여 일치하는 번호를 set으로 반환한다.
+     */
+    private fun getMatchedNumberSet(otherLotto: Lotto): Set<Int> {
+        return winningLotto.numbers.intersect(otherLotto.numbers)
     }
 
+    /**
+     * otherLotto 와 winningLotto의 번호를 비교하여 일치하는 번호를 출력하고, 당첨 결과를 반환한다.
+     */
     fun checkLottoResult(otherLotto: Lotto): WinningPrize {
-        val matchedNumbers = getMatchedNumbers(otherLotto)
+        val matchedNumbers = getMatchedNumberSet(otherLotto)
         showMatchedNumbers(matchedNumbers)
         return when (matchedNumbers.size) {
             6 -> FIRST
@@ -31,6 +43,9 @@ object WinningLotto {
         }
     }
 
+    /**
+     * 일치하는 번호를 출력한다.
+     */
     private fun showMatchedNumbers(matchedNumbers: Set<Int>) {
         print("당첨 번호 : ${matchedNumbers.sorted().joinToString(" ")}  ")
     }
