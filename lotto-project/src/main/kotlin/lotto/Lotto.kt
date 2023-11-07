35b78cbd8be5b3ab29c6fdc963b69f60fa80ae05
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-07 12:44:09 +0900

    Refactor: 생성자 속 람다 인자 이름 변경 assignAction -> generateStrategy

--- lotto-project/src/main/kotlin/lotto/Lotto.kt
+++ lotto-project/src/main/kotlin/lotto/Lotto.kt
@@ -1,12 +1,22 @@
 package lotto
 
-class Lotto(numOfLottos: Int = 6, assignAction: (lotto: Lotto, numOfLottos: Int) -> Unit) {
+/**
+ * Lotto
+ * 로또를 생성하는 객체
+ * 로또 번호를 가지고 있다. (Set)
+ * 로또 번호를 생성하는 방법을 전략으로 받아 생성한다. (Strategy Pattern)
+ * generateStrategy: @see [lottomachine.LottoMachineStrategy.generateLotto]
+ */
+class Lotto(numOfLottos: Int = 6, generateStrategy: (lotto: Lotto, numOfLottos: Int) -> Unit) {
     val numbers = mutableSetOf<Int>()
 
     init {
-        assignAction(this, numOfLottos)
+        generateStrategy(this, numOfLottos)
     }
 
+    /**
+     * 로또 번호를 출력한다.
+     */
     fun showLottoNumbers(prefix: String = "", postfix: String = "\n") {
         print("$prefix${numbers.sorted().joinToString()}$postfix")
     }
