c2bd0d920ae746accae15e815fc2441b5773da52
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-07 12:43:01 +0900

    Remove: 반복되거나 의미없는 코드 제거
    override fun drawLottos(user: User): List<Lotto> -> 사용되지 않아 제거
    override fun drawLottos(user: User, numOfLottos: Int): List<Lotto> -> 인터페이스에서 구현

--- lotto-project/src/main/kotlin/lottomachine/AutomaticLottoMachineStrategy.kt
+++ lotto-project/src/main/kotlin/lottomachine/AutomaticLottoMachineStrategy.kt
@@ -1,40 +1,30 @@
 package lottomachine
 
 import lotto.Lotto
-import user.User
 import kotlin.random.Random
 
 /**
  * @author : Unagi_zoso
  * @date : 2023-11-06
  */
+
+/**
+ * AutomaticLottoMachineStrategy
+ * LottoMachineStrategy 구현체
+ */
 class AutomaticLottoMachineStrategy : LottoMachineStrategy {
     override val COST_OF_ONE_LOTTO: Int
         get() = 1000
 
-    override fun drawLottos(user: User): List<Lotto> {
-        val numOfLottos = user.currentMoney / COST_OF_ONE_LOTTO
-        val lottos = mutableListOf<Lotto>()
-        repeat(numOfLottos) {
-            lottos.add(generateLotto())
-        }
-        return lottos
-    }
-
-    override fun drawLottos(user: User, numOfLottos: Int): List<Lotto> {
-        val lottos = mutableListOf<Lotto>()
-        repeat(numOfLottos) {
-            lottos.add(generateLotto())
-        }
-        return lottos
-    }
-
+    /**
+     * 자동 로또를 생성하여 반환
+     * 1 ~ 45 사이의 숫자를 랜덤으로 선택하여 로또를 생성한다.
+     * 중복된 숫자는 제외한다. (Set을 이용)
+     */
     override fun generateLotto(): Lotto {
-        return Lotto { lotto, numOfSelection ->
-            var nextNum: Int
-            while (lotto.numbers.size != numOfSelection) {
-                nextNum = Random.nextInt(1, 46)
-                lotto.numbers.add(nextNum)
+        return Lotto { lotto, numOfLottos ->
+            while (lotto.numbers.size != numOfLottos) {
+                lotto.numbers.add(Random.nextInt(1, 46))
             }
         }
     }
