40a1247ae70115f4e152c844b8e2a45d261947c4
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 12:10:20 +0900

    Feat: Lotto의 기본 생성자 인자 변경
    전략 객체의 override 함수(Lotto에 들어갈 숫자를 반환)를 받아 Lotto를 생성하는 방식에서, Lotto 숫자들 자체를 받아 생성하도록 바꾸었다.
    기존 전략 객체는 Lotto 안의 숫자를 생성하는 것이 아닌 전략에 따라 Lotto를 생성하도록 변경하였다.

--- lotto-project/src/main/kotlin/domain/lotto/Lotto.kt
+++ lotto-project/src/main/kotlin/domain/lotto/Lotto.kt
@@ -6,11 +6,8 @@ import java.util.SortedSet
  * Lotto
  * 로또를 생성하는 객체
  * 로또 번호를 가지고 있다. (SortedSet)
- * 로또 번호를 생성하는 방법을 전략으로 받아 생성한다. (Strategy Pattern)
- * generateStrategy: @see [domain.lottomachine.AutomaticLottoMachineStrategy.generateLotto]
  */
-data class Lotto(val generateStrategy: () -> SortedSet<Int>) {
-    val numbers = generateStrategy()
+data class Lotto(val numbers: SortedSet<Int>) {
     companion object Constants {
         const val NUM_OF_LOTTO_NUMBERS = 6
         const val MIN_LOTTO_NUMBER = 1
