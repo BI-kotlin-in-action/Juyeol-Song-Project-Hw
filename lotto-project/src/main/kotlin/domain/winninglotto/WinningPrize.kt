f9a33cdc69df51cad1592895a0982d948d7fc486
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 21:56:57 +0900

    Feat: @JvmStatic 어노테이션 추가, getPrize() 로직 개선

--- lotto-project/src/main/kotlin/domain/winninglotto/WinningPrize.kt
+++ lotto-project/src/main/kotlin/domain/winninglotto/WinningPrize.kt
@@ -9,27 +9,23 @@ package domain.winninglotto
  * WinningPrize
  * 당첨 등수와 당첨 금액을 가지고 있는 enum class
  */
-enum class WinningPrize(val level: String, val prizeAmount: Int) {
-    FIRST("1등", 100_000),
-    SECOND("2등", 5_000),
-    THIRD("3등", 100),
-    FOURTH("4등", 5),
-    NOPE("낙", 0), ;
+enum class WinningPrize(val level: String, val matchCount: Int, val prizeAmount: Int) {
+    FIRST("1등", 6, 100_000),
+    SECOND("2등", 5, 5_000),
+    THIRD("3등", 4, 100),
+    FOURTH("4등", 3, 5),
+    NOPE("낙", 0, 0), ;
 
     companion object Converter {
+        @JvmStatic
         fun getPrize(numOfMatchedNumbers: Int): WinningPrize {
-            return when (numOfMatchedNumbers) {
-                6 -> FIRST
-                5 -> SECOND
-                4 -> THIRD
-                3 -> FOURTH
-                else -> NOPE
-            }
+            return WinningPrize.values().find { it.matchCount == numOfMatchedNumbers } ?: NOPE
         }
 
         /**
          * 당첨 번호와 일치하는 정도(size)들의 리스트를 당첨 보상 (WinningPrize) 리스트로 반환한다.
          */
+        @JvmStatic
         fun convertToPrizeList(matchedSizeList: List<Int>): List<WinningPrize> {
             return matchedSizeList.map(::getPrize)
         }
