a7043efd5ec709fc05898852fb0aa8fb517bd8a8
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 12:18:34 +0900

    Feat: 로또 구매, 당첨금액 환수 기능 추가. lottos 프로퍼티 타입 변경 (var -> val, List<> -> MutableList<>), 로또 구매 시 전략 객체 사용

--- lotto-project/src/main/kotlin/domain/user/User.kt
+++ lotto-project/src/main/kotlin/domain/user/User.kt
@@ -1,16 +1,39 @@
 package domain.user
 
 import domain.lotto.Lotto
-import domain.lottomachine.LottoMachineStrategy.Constants.COST_OF_ONE_LOTTO
-import view.user.UserView
+import domain.lottogenerator.LottoGenerator.Companion.generateLottos
+import domain.winninglotto.WinningPrize
+import utils.lottomachine.AutomaticLottoMachineStrategy
+import utils.lottomachine.LottoMachineStrategy.Constants.COST_OF_ONE_LOTTO
+import utils.lottomachine.ManualLottoMachineStrategy
 
 /**
  * @author : Unagi_zoso
  * @date : 2023-11-06
  */
 data class User(
-    var currentMoney: Int = UserView().inputStartMoneyWithValidation(),
+    var currentMoney: Int,
     var earnedMoney: Int = 0,
     val maxNumOfLottos: Int = currentMoney / COST_OF_ONE_LOTTO,
-    var lottos: List<Lotto> = listOf(),
-)
+    val lottos: MutableList<Lotto> = mutableListOf(),
+) {
+    /**
+     * 1. 전재산을 털어 로또를 삽니다.
+     * 2. 수동으로 구매할 로또의 개수를 입력받습니다.
+     * 3. 수동으로 구매할 로또의 개수만큼 로또를 생성합니다.
+     * 4. 나머지는 자동으로 생성합니다.
+     */
+    fun buyManualAndAutonomousLottos(numOfManuals: Int) {
+        currentMoney %= COST_OF_ONE_LOTTO
+
+        lottos += generateLottos(ManualLottoMachineStrategy::generateLotto, numOfManuals)
+        lottos += generateLottos(AutomaticLottoMachineStrategy::generateLotto, maxNumOfLottos - numOfManuals)
+    }
+
+    /**
+     * 당첨 결과를 통해 총 당첨금액을 지급받는다.
+     */
+    fun receiveAllPrizeAmount(resultsOfAllLottos: List<WinningPrize>) {
+        earnedMoney += resultsOfAllLottos.sumOf(WinningPrize::prizeAmount)
+    }
+}
