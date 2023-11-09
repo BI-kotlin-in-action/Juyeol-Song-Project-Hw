e73d78006749b2917a6134f9f60d4723bb14730d
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 17:54:17 +0900

    Refactor: LottoMachineStrategy.generateLotto 매개변수 변경으로 인한 수정
    fun generateLotto(): Lotto -> fun generateLotto(numbers: SortedSet<Int>?): Lotto

--- lotto-project/src/main/kotlin/utils/lottomachine/AutomaticLottoMachineStrategy.kt
+++ lotto-project/src/main/kotlin/utils/lottomachine/AutomaticLottoMachineStrategy.kt
@@ -4,6 +4,7 @@ import domain.lotto.Lotto
 import domain.lotto.Lotto.Constants.MAX_LOTTO_NUMBER
 import domain.lotto.Lotto.Constants.MIN_LOTTO_NUMBER
 import domain.lotto.Lotto.Constants.NUM_OF_LOTTO_NUMBERS
+import java.util.SortedSet
 
 /**
  * @author : Unagi_zoso
@@ -22,8 +23,9 @@ class AutomaticLottoMachineStrategy {
          * 자동 로또를 생성하여 반환
          * 1 ~ 45 사이의 숫자를 랜덤으로 선택하여 로또를 생성한다.
          * 1..45 를 shuffle 한 후, 앞에서 6개를 뽑아서 sortedSet으로 반환한다.
+         * 패러미터 numbers는 사용되지 않는다.
          */
-        override fun generateLotto(): Lotto {
+        override fun generateLotto(numbers: SortedSet<Int>?): Lotto {
             return Lotto(numberOneToFortyFive.shuffled().take(NUM_OF_LOTTO_NUMBERS).toSortedSet())
         }
     }
