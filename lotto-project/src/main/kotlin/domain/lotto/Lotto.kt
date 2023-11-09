29a403d281a7c2474c346998d232276ee20829cf
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 17:55:34 +0900

    Feat: numbers 일급컬렉션 private 접근제어자 추가 및 Immutable 반환 접근자 추가

--- lotto-project/src/main/kotlin/domain/lotto/Lotto.kt
+++ lotto-project/src/main/kotlin/domain/lotto/Lotto.kt
@@ -5,9 +5,14 @@ import java.util.SortedSet
 /**
  * Lotto
  * 로또를 생성하는 객체
- * 로또 번호를 가지고 있다. (SortedSet)
+ * 로또 번호를 가지고 있다. (private val SortedSet)
+ * 접근 시 getNumbers() 메서드를 통해 Set으로 반환한다.
  */
-data class Lotto(val numbers: SortedSet<Int>) {
+data class Lotto(private val numbers: SortedSet<Int>) {
+    fun getNumbers(): Set<Int> {
+        return numbers
+    }
+
     companion object Constants {
         const val NUM_OF_LOTTO_NUMBERS = 6
         const val MIN_LOTTO_NUMBER = 1
