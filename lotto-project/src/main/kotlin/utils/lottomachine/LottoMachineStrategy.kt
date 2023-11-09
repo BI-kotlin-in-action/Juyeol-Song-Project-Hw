9a20f36bed1b9a0e60a340b151f459bc8966216b
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 18:00:58 +0900

    Refactor: generateLotto() 매개변수 추가. 외부로부터 로또 번호를 주입 받을 수 있다.

--- lotto-project/src/main/kotlin/utils/lottomachine/LottoMachineStrategy.kt
+++ lotto-project/src/main/kotlin/utils/lottomachine/LottoMachineStrategy.kt
@@ -1,6 +1,7 @@
 package utils.lottomachine
 
 import domain.lotto.Lotto
+import java.util.SortedSet
 
 /**
  * @author : Unagi_zoso
@@ -17,9 +18,10 @@ interface LottoMachineStrategy {
 
     /**
      * 전략에 따라 로또를 생성하여 반환
+     * 외부로부터 번호가 생성되어 주입될 시 numbers로 받는다. ex) ManualLottoMachineStrategy
      * @see [utils.lottomachine.AutomaticLottoMachineStrategy.generateLotto]
      * @see [utils.lottomachine.ManualLottoMachineStrategy.generateLotto]
      * @see [domain.lottogenerator.LottoGenerator] - strategy client 객체
      */
-    fun generateLotto(): Lotto
+    fun generateLotto(numbers: SortedSet<Int>?): Lotto
 }
