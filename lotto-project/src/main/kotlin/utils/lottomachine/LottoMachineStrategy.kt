44f0daca3e449b239cc6f380639b095b7570b966
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 21:59:23 +0900

    Feat: generateLotto 패러미 제거)

--- lotto-project/src/main/kotlin/utils/lottomachine/LottoMachineStrategy.kt
+++ lotto-project/src/main/kotlin/utils/lottomachine/LottoMachineStrategy.kt
@@ -1,7 +1,6 @@
 package utils.lottomachine
 
 import domain.lotto.Lotto
-import java.util.SortedSet
 
 /**
  * @author : Unagi_zoso
@@ -18,10 +17,9 @@ interface LottoMachineStrategy {
 
     /**
      * 전략에 따라 로또를 생성하여 반환
-     * 외부로부터 번호가 생성되어 주입될 시 numbers로 받는다. ex) ManualLottoMachineStrategy
      * @see [utils.lottomachine.AutomaticLottoMachineStrategy.generateLotto]
      * @see [utils.lottomachine.ManualLottoMachineStrategy.generateLotto]
      * @see [domain.lottogenerator.LottoGenerator] - strategy client 객체
      */
-    fun generateLotto(numbers: SortedSet<Int>?): Lotto
+    fun generateLotto(): Lotto
 }
