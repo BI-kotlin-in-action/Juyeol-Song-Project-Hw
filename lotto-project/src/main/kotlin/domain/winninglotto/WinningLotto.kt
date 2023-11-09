c72167eeaee4e97811f1b41f9a6d5b813461a51e
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 21:56:27 +0900

    Feat: @JvmStatic 어노테이션 추가

--- lotto-project/src/main/kotlin/domain/winninglotto/WinningLotto.kt
+++ lotto-project/src/main/kotlin/domain/winninglotto/WinningLotto.kt
@@ -17,11 +17,13 @@ import java.util.SortedSet
  * 당첨 번호를 생성하고, 당첨 번호와 비교하여 당첨 결과를 반환한다.
  */
 object WinningLotto {
-    val winningLotto = generateOneLotto(null, AutomaticLottoMachineStrategy::generateLotto)
+    @JvmStatic
+    val winningLotto = generateOneLotto(AutomaticLottoMachineStrategy::generateLotto)
 
     /**
      * otherLotto와 당첨 번호를 비교하여 일치하는 번호를 SortedSet으로 반환한다.
      */
+    @JvmStatic
     private fun getMatchedNumberSet(otherLotto: Lotto): SortedSet<Int> {
         return (winningLotto.getNumbers() intersect otherLotto.getNumbers()).toSortedSet()
     }
@@ -29,6 +31,7 @@ object WinningLotto {
     /**
      * user가 가진 모든 로또를 각 각 당첨 번호와 비교해 일치하는 번호들을 반환한다.
      */
+    @JvmStatic
     fun getMatchedNumbersList(user: User): List<SortedSet<Int>> {
         return user.getLottos().map(::getMatchedNumberSet)
     }
