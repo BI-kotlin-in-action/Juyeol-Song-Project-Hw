f81421911fe964e5d57c515a1c95e5c0f5ad1554
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 21:57:47 +0900

    Feat: @JvmStatic 어노테이션 추가, generateLotto 패러미터 제거

--- lotto-project/src/main/kotlin/utils/lottomachine/AutomaticLottoMachineStrategy.kt
+++ lotto-project/src/main/kotlin/utils/lottomachine/AutomaticLottoMachineStrategy.kt
@@ -4,7 +4,6 @@ import domain.lotto.Lotto
 import domain.lotto.Lotto.Constants.MAX_LOTTO_NUMBER
 import domain.lotto.Lotto.Constants.MIN_LOTTO_NUMBER
 import domain.lotto.Lotto.Constants.NUM_OF_LOTTO_NUMBERS
-import java.util.SortedSet
 
 /**
  * @author : Unagi_zoso
@@ -17,15 +16,16 @@ import java.util.SortedSet
  */
 class AutomaticLottoMachineStrategy {
     companion object : LottoMachineStrategy {
+        @JvmStatic
         private val numberOneToFortyFive = (MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER)
 
         /**
          * 자동 로또를 생성하여 반환
          * 1 ~ 45 사이의 숫자를 랜덤으로 선택하여 로또를 생성한다.
          * 1..45 를 shuffle 한 후, 앞에서 6개를 뽑아서 sortedSet으로 반환한다.
-         * 패러미터 numbers는 사용되지 않는다.
          */
-        override fun generateLotto(numbers: SortedSet<Int>?): Lotto {
+        @JvmStatic
+        override fun generateLotto(): Lotto {
             return Lotto(numberOneToFortyFive.shuffled().take(NUM_OF_LOTTO_NUMBERS).toSortedSet())
         }
     }
