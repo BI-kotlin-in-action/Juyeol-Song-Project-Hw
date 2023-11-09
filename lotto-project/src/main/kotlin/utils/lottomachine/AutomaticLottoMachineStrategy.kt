5e8630d1928492c7903d9dec28cc8d53ec3d1505
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-09 12:05:34 +0900

    Feat: override generateLotto() 반환타입 변경으로 인한 수정, utils 디렉터리로 이동

---
+++ lotto-project/src/main/kotlin/utils/lottomachine/AutomaticLottoMachineStrategy.kt
@@ -0,0 +1,30 @@
+package utils.lottomachine
+
+import domain.lotto.Lotto
+import domain.lotto.Lotto.Constants.MAX_LOTTO_NUMBER
+import domain.lotto.Lotto.Constants.MIN_LOTTO_NUMBER
+import domain.lotto.Lotto.Constants.NUM_OF_LOTTO_NUMBERS
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-11-06
+ */
+
+/**
+ * AutomaticLottoMachineStrategy
+ * LottoMachineStrategy 구현체
+ */
+class AutomaticLottoMachineStrategy {
+    companion object : LottoMachineStrategy {
+        private val numberOneToFortyFive = (MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER)
+
+        /**
+         * 자동 로또를 생성하여 반환
+         * 1 ~ 45 사이의 숫자를 랜덤으로 선택하여 로또를 생성한다.
+         * 1..45 를 shuffle 한 후, 앞에서 6개를 뽑아서 sortedSet으로 반환한다.
+         */
+        override fun generateLotto(): Lotto {
+            return Lotto(numberOneToFortyFive.shuffled().take(NUM_OF_LOTTO_NUMBERS).toSortedSet())
+        }
+    }
+}
