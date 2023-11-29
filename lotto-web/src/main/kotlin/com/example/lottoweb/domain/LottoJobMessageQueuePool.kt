a58eb5f26b55466df0b1b5c7047bd2f1e5db64c9
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-30 05:03:45 +0900

    Refactor: queueWorkingCounter record 초기화 부분 제거, QUEUE_POOL_SIZE 크기 수정 1000 -> 100

--- lotto-web/src/main/kotlin/com/example/lottoweb/domain/LottoJobMessageQueuePool.kt
+++ lotto-web/src/main/kotlin/com/example/lottoweb/domain/LottoJobMessageQueuePool.kt
@@ -1,6 +1,5 @@
 package com.example.lottoweb.domain
 
-import com.example.lottoweb.service.QueueWorkingCounterService
 import org.springframework.context.annotation.Configuration
 import java.util.LinkedList
 
@@ -9,19 +8,13 @@ import java.util.LinkedList
  * @since 2023-11-24
  */
 // 10분을 넘어가는 순간에도 로또를 구매할 수 있게 회차별로 queue를 미리 만들어두어 담을 수 있게 하였습니다.
-// 현재 QUEUE_POOL_SIZE는 1000이며 적어도 1000개의 queue를 순회할 동안 가장 먼저 시작한 queue는 새로운 회차의
+// 현재 QUEUE_POOL_SIZE는 100이며 적어도 100개의 queue를 순회할 동안 가장 먼저 시작한 queue는 새로운 회차의
 // 작업을 저장할 때 자신이 쓰던 queue를 비워 막히는 일이 없길 기대하여 선정한 값입니다.
 @Configuration
-class LottoJobMessageQueuePool(
-    queueWorkingCounterService: QueueWorkingCounterService,
-) {
-    init {
-        queueWorkingCounterService.initQueueWorkingFlag(QUEUE_POOL_SIZE)
-    }
-
+class LottoJobMessageQueuePool() {
     companion object {
         @JvmStatic
-        val QUEUE_POOL_SIZE = 1000
+        val QUEUE_POOL_SIZE = 100
 
         @JvmStatic
         val queuePool = List<LinkedList<LottoJobMessage>>(QUEUE_POOL_SIZE) { LinkedList() }
