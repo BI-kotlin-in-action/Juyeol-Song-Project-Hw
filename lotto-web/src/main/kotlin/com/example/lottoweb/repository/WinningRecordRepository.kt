cb64df516e686d0ce854ad2ba7f2e5de5765eb51
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-30 02:49:34 +0900

    Feat: WinningLotto entity, repository, service, controller 클래스 추가

--- lotto-web/src/main/kotlin/com/example/lottoweb/repository/WinningRecordRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lottoweb.repository
-
-import com.example.lottoweb.domain.model.WinningRecord
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface WinningRecordRepository : JpaRepository<WinningRecord, Long> {
-    fun findByRound(round: Int): WinningRecord
-}
