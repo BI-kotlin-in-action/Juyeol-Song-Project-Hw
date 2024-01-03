a19e3a1e2d6696fea08acba0ed52570b7c28cae0
Unagi_zoso <Unagi.zoso@gmail.com>
2024-01-04 02:49:10 +0900

    Refactor: HttpErrorResponse 형식 변화에 따른 수정

--- lotto-web/src/main/kotlin/com/example/lottoweb/exception/HttpErrorResponse.kt
+++ lotto-web/src/main/kotlin/com/example/lottoweb/exception/HttpErrorResponse.kt
@@ -1,6 +1,11 @@
 package com.example.lottoweb.exception
 
+import org.hibernate.exception.ConstraintViolationException
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
 
 /**
  * @author Unagi_zoso
@@ -8,24 +13,30 @@ import org.springframework.http.HttpStatus
  */
 // 예외 발생 시 공통 예외 Response
 data class HttpErrorResponse(
-    val statusCode: HttpStatus,
     val message: String,
 ) {
     companion object {
         @JvmStatic
-        fun of(exception: RuntimeException) =
-            HttpErrorResponse(
-                statusCode = when (exception) {
-                    is LoginMissingException -> HttpStatus.BAD_REQUEST
-                    is NotEnoughMoneyException -> HttpStatus.BAD_REQUEST
-                    is DuplicatedUsernameException -> HttpStatus.BAD_REQUEST
-                    is PasswordNotMatchException -> HttpStatus.BAD_REQUEST
-                    is InvalidLottoBuyRequestException -> HttpStatus.BAD_REQUEST
-                    is UsernameNotFoundException -> HttpStatus.BAD_REQUEST
-                    is FrequentRequestException -> HttpStatus.BAD_REQUEST
-                    else -> HttpStatus.INTERNAL_SERVER_ERROR
-                },
-                message = exception.message ?: "알 수 없는 에러가 발생했습니다.",
+        fun toResponseEntity(exception: Exception): ResponseEntity<HttpErrorResponse> {
+            val statusCode = when (exception) {
+                is LoginMissingException -> BAD_REQUEST
+                is NotEnoughMoneyException -> BAD_REQUEST
+                is DuplicatedUsernameException -> BAD_REQUEST
+                is PasswordNotMatchException -> BAD_REQUEST
+                is InvalidLottoNumbersException -> BAD_REQUEST
+                is UsernameNotFoundException -> BAD_REQUEST
+                is HttpMessageNotReadableException -> BAD_REQUEST
+                is ConstraintViolationException -> BAD_REQUEST
+                is MethodArgumentNotValidException -> BAD_REQUEST
+                else -> HttpStatus.INTERNAL_SERVER_ERROR
+            }
+
+            return ResponseEntity(
+                HttpErrorResponse(
+                    message = exception.message ?: "알 수 없는 에러가 발생했습니다.",
+                ),
+                statusCode
             )
+        }
     }
 }
