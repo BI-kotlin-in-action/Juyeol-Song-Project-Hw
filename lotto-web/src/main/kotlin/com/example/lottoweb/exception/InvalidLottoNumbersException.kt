0d4f1fa330a451b22c6958eba8f3f2dab805f924
Unagi_zoso <Unagi.zoso@gmail.com>
2024-01-04 02:54:11 +0900

    Refactor: 주석 제거

--- lotto-web/src/main/kotlin/com/example/lottoweb/exception/InvalidLottoNumbersException.kt
+++ lotto-web/src/main/kotlin/com/example/lottoweb/exception/InvalidLottoNumbersException.kt
@@ -3,9 +3,6 @@ package com.example.lottoweb.exception
 import org.springframework.http.HttpStatus
 
 // 로또 번호가 유효하지 않을 때 발생하는 예외
-/**
- * @see com.example.lottoweb.utils.validator.LottoBuyRequestValidator
- */
 class InvalidLottoNumbersException(
     val statusCode: HttpStatus = HttpStatus.BAD_REQUEST,
     override val message: String = "로또 번호를 확인해주세요.",
