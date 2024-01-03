be75d9b311db513a40e6b9791ef196f471e233b3
Unagi_zoso <Unagi.zoso@gmail.com>
2024-01-04 03:46:56 +0900

    Feat: version 프로퍼티 추가

--- lotto-web/src/main/kotlin/com/example/lottoweb/domain/model/User.kt
+++ lotto-web/src/main/kotlin/com/example/lottoweb/domain/model/User.kt
@@ -11,17 +11,12 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import jakarta.persistence.UniqueConstraint
+import jakarta.persistence.Version
 import org.springframework.security.crypto.password.PasswordEncoder
 
 /**
  * @author Unagi_zoso
  * @since 2023-11-22
- */
-/**
- * user의 잔금을 저장하는 balance를 따로 account라는 테이블로 분리하는게 더 user를 user답게 만들지 않을까 생각했습니다.
- * 초반에 프로그램을 만들 땐 두 테이블로 분리하였습니다. 하지만 결국 user를 통해 참조 (혹은 조인)하여
- * 처리를 하게되는 것을 보고 어색함이 느꼈습니다. 이번의 경우 user의 account가 다수 개를 허용하지 않기에
- * 그냥 user라는 하나의 테이블에서 balance를 다루기로 하였습니다.
  */
 @Entity(name = "user")
 @Table(
@@ -39,6 +34,8 @@ data class User(
     var balance: Long,
     @Enumerated(EnumType.STRING)
     val role: Role,
+    @Version
+    val version: Long = 0,
 ) {
     fun depositBalance(amount: Long) {
         balance += amount
