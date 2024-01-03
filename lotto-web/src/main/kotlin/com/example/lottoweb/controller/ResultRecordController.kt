1655430d3d884eb9d657fda2b58275b6d7f6478f
Unagi_zoso <Unagi.zoso@gmail.com>
2024-01-04 03:43:46 +0900

    Refactor: getLottoResults 메소드 username 인자타입 변경 pathvariable -> requestparam, CoolDown 제거

--- lotto-web/src/main/kotlin/com/example/lottoweb/controller/ResultRecordController.kt
+++ lotto-web/src/main/kotlin/com/example/lottoweb/controller/ResultRecordController.kt
@@ -1,14 +1,11 @@
 package com.example.lottoweb.controller
 
-import com.example.lottoweb.dto.LottoResultsResponse
 import com.example.lottoweb.service.ResultRecordService
-import com.example.lottoweb.utils.annotation.CoolDown
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Positive
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -28,18 +25,15 @@ class ResultRecordController(
      * @param username 사용자 이름
      * @param round 로또 회차
      */
-    @CoolDown
-    @GetMapping("/username/{username}")
+    @GetMapping
     fun getLottoResults(
         @NotEmpty
-        @PathVariable
+        @RequestParam("username")
         username: String,
         @Positive
         @RequestParam("round")
         round: Int,
-    ): ResponseEntity<LottoResultsResponse> {
-        return ResponseEntity.ok(
-            resultRecordService.getResultRecord(round, username),
-        )
-    }
+    ) = ResponseEntity.ok(
+        resultRecordService.getResultRecord(round, username),
+    )
 }
