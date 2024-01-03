08435cd4bec3b8f9566bf6325cd4a00662d35b71
Unagi_zoso <Unagi.zoso@gmail.com>
2024-01-04 03:36:10 +0900

    Feat: QueueWorkingCounter 엔티티 version 프로퍼티 추가

--- lotto-web/src/main/kotlin/com/example/lottoweb/domain/model/QueueWorkingCounter.kt
+++ lotto-web/src/main/kotlin/com/example/lottoweb/domain/model/QueueWorkingCounter.kt
@@ -2,6 +2,7 @@ package com.example.lottoweb.domain.model
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Version
 
 /**
  * @author Unagi_zoso
@@ -9,6 +10,7 @@ import jakarta.persistence.Id
  */
 // 해당 회차의 로또를 생성하는 작업 수를 관리하는 테이블
 // 해당 테이블의 용도는 아래 LottoSchedulerService를 참고해주세요.
+// resources/initial_data.sql 에서 초기값을 설정해주었습니다.
 /**
  * @see com.example.lottoweb.service.LottoSchedulerService
  */
@@ -16,6 +18,8 @@ import jakarta.persistence.Id
 data class QueueWorkingCounter(
     @Id
     val queueRound: Int,
+    @Version
+    val version: Long = 0,
     var counter: Long = 0,
 ) {
     fun increaseCounter() {
