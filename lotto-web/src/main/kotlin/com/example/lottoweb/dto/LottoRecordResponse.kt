a3e6af69b2f30cc9e4bc5f8132cd0c3330ceb827
Unagi_zoso <Unagi.zoso@gmail.com>
2024-01-04 20:58:02 +0900

    Refactor: lottoNumbers 자료형 변경 String -> LottoNumbers (JsonSerialize 사용)

--- lotto-web/src/main/kotlin/com/example/lottoweb/dto/LottoRecordResponse.kt
+++ lotto-web/src/main/kotlin/com/example/lottoweb/dto/LottoRecordResponse.kt
@@ -1,6 +1,9 @@
 package com.example.lottoweb.dto
 
+import com.example.lottoweb.domain.LottoNumbers
 import com.example.lottoweb.domain.model.LottoRecord
+import com.example.lottoweb.utils.jsonbind.LottoNumbersSerializer
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 
 // 단일 구매한 로또 정보를 반환 받는 DTO
 // 실제론 다수 개의 정보를 반환 받기에 일급컬렉션 느낌의 LottoRecordsResponse를 실질적으로 사용한다.
@@ -11,11 +14,14 @@ data class LottoRecordResponse(
     val round: Int,
     val currentCounterOfThisRound: Long,
     val username: String,
-    val lottoNumbers: String,
+    @field: JsonSerialize(using = LottoNumbersSerializer::class)
+    val lottoNumbers: LottoNumbers,
 ) {
+    constructor() : this(0, 0, "", LottoNumbers.from(listOf()))
+
     companion object {
         @JvmStatic
-        fun from(lottoRecord: LottoRecord, username: String, lottoNumbers: String) = LottoRecordResponse(
+        fun from(lottoRecord: LottoRecord, username: String, lottoNumbers: LottoNumbers) = LottoRecordResponse(
             round = lottoRecord.round,
             currentCounterOfThisRound = lottoRecord.currentCounterOfThisRound,
             username = username,
